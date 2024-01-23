af73a81bd5fb1be8baa9fe75a85bdd156cd3c25c
Ruolinnnnn <rl745@cam.ac.uk>
2024-01-23 12:09:20 +0000

    1A

--- Station.py
+++ Station.py
@@ -1,3 +1,4 @@
 class MonitoringStation:
-    def _init_(self,Station_ID, Measurement_ID,Name ):
+    def _init_(self,Station_ID, Measurement_ID, Name, Geographic_coordinate, Typical_low_high_levels, River_on_which_the_station_is_located, Closest_town_to_the_station):
         
+        def()
