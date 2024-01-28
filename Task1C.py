a9315e962eb68869c3ddca619983ad247c1a1188
Ruolinnnnn <rl745@cam.ac.uk>
2024-01-28 13:23:09 +0000

    interface changed

--- Task1C.py
+++ Task1C.py
@@ -1,13 +1,21 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_within_radius
 
-stations = build_station_list()
 
-centre=(52.2053, 0.1218)
-r=10
-station_in=stations_within_radius(stations,centre,r)
-station_in_data=[]
-for station in station_in:
-    station_in_data.append(station.name)
 
-print(station_in_data)
+
+
+def run():
+    stations = build_station_list()
+
+    centre=(52.2053, 0.1218)
+    r=10
+    station_in=stations_within_radius(stations,centre,r)
+    station_in_data=[]
+    for station in station_in:
+        station_in_data.append(station.name)
+
+    print(station_in_data)
+    
+if __name__ == "__main__":
+    run()
--- Task1D.py
+++ Task1D.py
@@ -2,11 +2,18 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
 
-stations = build_station_list()
 
-rivers1=sorted(rivers_with_station(stations))
-print(len(rivers1),"stations, first 10:",rivers1[:10])
 
-rivers2=stations_by_river(stations)
-print(sorted(rivers2["River Aire"]))
-print(sorted(rivers2["River Cam"]))
+
+def run():
+    stations = build_station_list()
+
+    rivers1=sorted(rivers_with_station(stations))
+    print(len(rivers1),"stations, first 10:",rivers1[:10])
+
+    rivers2=stations_by_river(stations)
+    print(sorted(rivers2["River Aire"]))
+    print(sorted(rivers2["River Cam"]))
+    
+if __name__ == "__main__":
+    run()
