8349b2dcd6eb8accafe9e42916c685fc7deea5c1
Ruolinnnnn <rl745@cam.ac.uk>
2024-02-20 14:09:50 +0000

    Adopt Task2G_alter

--- Task2G.py
+++ Task2G.py
@@ -8,74 +8,81 @@ from floodsystem.flood import stations_level_over_threshold
 def run():
     
     # Get stations data with water level
-    # Fetching all stations data will take very very long so we test for 100 of them
-    stations=build_station_list()[:10]
+    # Fetching all 2200+ stations data will take very very long so we test for 100 of them
+    stations=build_station_list()[:100]
     update_water_levels(stations)
-    
-    # Relative water level of 2.0 for severe
-    severe=[]
-    for element in stations_level_over_threshold(stations,2.0):
-        severe.append (element[0])
-        
-    # Relative water level of 1.5-2.0 for high
-    high=[]
-    for element in stations_level_over_threshold(stations,1.5):
-        high.append(element[0])
-            
-    #  1.0-1.5 for moderate
-    moderate=[]
-    for element in stations_level_over_threshold(stations,1.0):
-        moderate.append(element[0])
-            
-    # Lower than 1.0 for low
-    low=stations
 
     
     # Consider highest water level predicted by curve-fitting in the next 3 days
     dt1=10  
     dt2=3
     p=4
+    towns={}
+    # Relative water level >2.0 for severe
+    severe=set()
+    # 1.5-2.0 for high
+    high=set()
+    # 1.0-1.5 for moderate
+    moderate=set()
+    # <1.0 for low
+    low=set()
+    
+
           
     for element in stations:
+        
+        # Safe fetch to avoid value error caused by corrupted data
         dates, levels = fetch_measure_levels_safe(element.measure_id, dt=datetime.timedelta(days=dt1))
         
         # Check if dates is a list of datetime.date
         if all(isinstance(date, datetime.date) for date in dates):
         # Check if levels is a list of float
             if all(isinstance(level, float) for level in levels):
+                # Check level and dates have equal length and are not empty
                 if len(dates)==len(levels)!=0:
-                    if hasattr(element, 'typical_range') and element.typical_range is not None:
-                        h=predict_highest_water_level(dates,levels,p,dt2)
-                        r=h/element.typical_range[1]
-                        if (h > element.latest_level):
-                            if r>2.0 and element not in severe:
-                                severe.append(element.name)
-            
-                            if 1.5<r<2.0 and element not in high:
-                                high.append(element.name)
-                
-                            if 1.0<r<2.0 and element not in moderate:
-                                moderate.append(element.name)
-                
-    # Subtract higher risk form lower risk
-    low=[element for element in low if element not in moderate and element not in high and element not in severe]
-    moderate=[element for element in moderate if element not in high and element not in severe]
-    high=[element for element in high if element not in severe]
+                    # Check the station has typical range and latest level
+                    if hasattr(element, 'typical_range') and element.typical_range is not None and hasattr(element, 'latest_level') and element.latest_level is not None:                      
+                        # predict the highest relative water level in next dt2 days
+                        predicted_relative_level=predict_highest_water_level(dates,levels,p,dt2)/element.typical_range[1]
+                        latest_relative_level=element.latest_level/element.typical_range[1]
+                        # Use the higher one of current level and predicted level to access the risk
+                        risk_key=max(predicted_relative_level,latest_relative_level)
+                        if risk_key>=2.0:
+                            risk="severe"
+                            severe.add(element.name)
+                        elif 1.5<risk_key<2.0:
+                            risk="high"
+                            high.add(element.name)
+                        elif 1.0<risk_key<=1.5:
+                            risk="moderate"
+                            moderate.add(element.name)
+                        else:
+                            risk="low"
+                            low.add(element.name)
+                        
+                        # Check the station has town, and generate a dict where the towns are keys to risk_key and risk
+                        if hasattr(element, 'town') and element.town is not None and element.town not in towns:
+                            towns[element.town]=(risk_key,risk)
+                        elif risk_key>towns[element.town][0]:
+                            towns[element.town]=(risk_key,risk)
+    
+    # Sort the dict based on risk_key, in descending order                    
+    sorted_towns = dict(sorted(towns.items(), key=lambda x: x[1][0], reverse=True))
+    top_10_risk_towns = dict(list(sorted_towns.items())[:10])
+    print(top_10_risk_towns)
+    print(severe)                        
+                        
+                        
+                            
+
+
     
     
-    # Find towns at severe risk
-    towns_at_severe_risk=[]
-    for element in severe:
-        for station in stations:
-            if station.name==element and station.town not in towns_at_severe_risk:
-                towns_at_severe_risk.append((station.town,station.latest_level/station.typical_range[1]))
-                
-    
-    print("Towns at severe risk are:",towns_at_severe_risk)
+
     
 
 if __name__ == "__main__":
-    print("*** Task 2F: CUED Part IA Flood Warning System ***")
+    print("*** Task 2G: CUED Part IA Flood Warning System ***")
     run()
             
         
